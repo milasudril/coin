c3277fea1aaebbfb1e8cff34e126e40c8737b1bf
Torbjörn Rathsman <milasudril@gmail.com>
2018-01-10 21:00:34 +0100

    Make Node a template (decouple from Element)

--- node.hpp
+++ node.hpp
@@ -3,25 +3,81 @@
 #ifndef SOXN_NODE_HPP
 #define SOXN_NODE_HPP
 
-#include "element.hpp"
-#include <type_traits>
+#include <utility>
 #include <cassert>
 
 namespace SoXN
 	{
+	enum class NodeType{Element, String};
+
+	template<NodeType type>	class GetAs;
+
+	template<>
+	struct GetAs<NodeType::String>
+		{
+		template<class Node>
+		static auto& get(Node& node) noexcept
+			{return *node.m_string;}
+
+		template<class Node>
+		static auto& get(const Node& node) noexcept
+			{return *node.m_string;}
+		};
+
+	template<>
+	struct GetAs<NodeType::Element>
+		{
+		template<class Node>
+		static auto& get(Node& node) noexcept
+			{return *node.m_element;}
+
+		template<class Node>
+		static auto& get(const Node& node) noexcept
+			{return *node.m_element;}
+		};
+
+	template<class ElementType,class StringType>
 	class Node
 		{
 		public:
-			enum class Type{Element, String};
+			typedef NodeType Type;
+			typedef ElementType element_type;
+			typedef StringType string_type;
 
 			auto type() const noexcept
 				{return m_type;}
 
-			explicit Node(const std::string& leaf):m_type(Type::String)
-				{m_string=new std::string(leaf);}
+			explicit Node(const StringType& leaf):m_type(Type::String)
+				{m_string=new StringType(leaf);}
 
-			explicit Node(const Element& e):m_type(Type::Element)
-				{m_element=new Element(e);}
+			explicit Node(const ElementType& e):m_type(Type::Element)
+				{m_element=new ElementType(e);}
+
+			Node(const Node& node)
+				{
+				m_type=node.type();
+				switch(node.type())
+					{
+					case Type::Element:
+						m_element=new ElementType(*node.m_element);
+						break;
+					case Type::String:
+						m_string=new StringType(*node.m_string);
+						break;
+					}
+				}
+
+			~Node() noexcept
+				{
+				switch(m_type)
+					{
+					case Type::Element:
+						delete m_element;
+					case Type::String:
+						delete m_string;
+					}
+				}
+
 
 			Node(Node&& node) noexcept:m_type(node.m_type),m_element(node.m_element)
 				{node.m_element=nullptr;}
@@ -33,20 +89,6 @@ namespace SoXN
 				return *this;
 				}
 
-			Node(const Node& node)
-				{
-				m_type=node.type();
-				switch(node.type())
-					{
-					case Type::Element:
-						m_element=new Element(*node.m_element);
-						break;
-					case Type::String:
-						m_string=new std::string(*node.m_string);
-						break;
-					}
-				}
-
 			Node& operator=(const Node& node)
 				{
 				Node temp(node);
@@ -54,62 +96,26 @@ namespace SoXN
 				return *this;
 				}
 
-			~Node()
-				{
-				switch(m_type)
-					{
-					case Type::Element:
-						delete m_element;
-					case Type::String:
-						delete m_string;
-					}
-				}
+			template<Type type>
+			auto& getAs() const noexcept
+				{return GetAs<type>::get(*this);}
 
-			template<Type t>
-			inline auto& getAs() const noexcept;
 
-			template<Type t>
-			auto& getAs() noexcept;
+			template<Type type>
+			auto& getAs() noexcept
+				{return GetAs<type>::get(*this);}
 
 		private:
 			Type m_type;
 			union
 				{
-				Element* m_element;
-				std::string* m_string;
+				ElementType* m_element;
+				StringType* m_string;
 				};
+
+			template<Type type>
+			friend class GetAs;
 		};
-
-		template<>
-		inline auto& Node::getAs<Node::Type::Element>() const noexcept
-			{
-			assert(m_type==Type::Element);
-			return *m_element;
-			}
-
-		template<>
-		inline auto& Node::getAs<Node::Type::String>() const noexcept
-			{
-			assert(m_type==Type::String);
-			return *m_string;
-			}
-
-
-		template<>
-		inline auto& Node::getAs<Node::Type::Element>() noexcept
-			{
-			assert(m_type==Type::Element);
-			return *m_element;
-			}
-
-		template<>
-		inline auto& Node::getAs<Node::Type::String>() noexcept
-			{
-			assert(m_type==Type::String);
-			return *m_string;
-			}
-
-
 	}
 
 #endif
